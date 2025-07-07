c1e9304fd6dae75cde376b965114558af8918c10
Brandon Khumalo <127394576+Brandonkhumalo@users.noreply.github.com>
2025-07-07 08:24:13 +0200

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,12 +4,14 @@ package { 'nginx':
   ensure => installed,
 }
 
+# Ensure nginx service is enabled and running
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  require    => Package['nginx'],
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
 }
 
+# Create a custom header config file
 file { '/etc/nginx/conf.d/custom_header.conf':
   ensure  => file,
   content => 'add_header X-Served-By $hostname;',
@@ -17,10 +19,14 @@ file { '/etc/nginx/conf.d/custom_header.conf':
   notify  => Service['nginx'],
 }
 
-# Ensure the config is included in the main nginx config
-exec { 'Include custom header in nginx config':
-  command => 'echo "include /etc/nginx/conf.d/*.conf;" >> /etc/nginx/nginx.conf',
-  unless  => 'grep -q "include /etc/nginx/conf.d/*.conf;" /etc/nginx/nginx.conf',
+# Make sure custom conf files are included in nginx.conf (idempotent)
+augeas { 'Include conf.d in nginx.conf':
+  context => '/files/etc/nginx/nginx.conf',
+  changes => [
+    'ins include after http',
+    'set include "include /etc/nginx/conf.d/*.conf;"',
+  ],
+  onlyif  => 'match /files/etc/nginx/nginx.conf/http/include[. = "include /etc/nginx/conf.d/*.conf;"] size == 0',
   require => File['/etc/nginx/conf.d/custom_header.conf'],
   notify  => Service['nginx'],
 }
